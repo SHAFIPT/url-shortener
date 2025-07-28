8fc98b3d961f7a42f20ae8521e678874086fef1d
SHAFIPT <shafipt077@gmail.com>
2025-07-28 14:07:41 +0530

    feat: add loading spinner in auth page

--- Frontend/src/pages/Auth.tsx
+++ Frontend/src/pages/Auth.tsx
@@ -20,7 +20,7 @@ import { useGoogleLogin } from '@react-oauth/google';
 const Auth = () => {
   const { mutate: loginMutate, isPending: loginLoading } = useLogin();
   const { mutate: registerMutate, isPending: registerLoading } = useRegister();
-  const { mutate: googleMutate } = useGoogleCodeLogin();
+  const { mutate: googleMutate ,isPending: googleLoading } = useGoogleCodeLogin();
   
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
@@ -100,7 +100,23 @@ const Auth = () => {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center relative overflow-hidden">
+    <>
+      {googleLoading && (
+        <div className="fixed inset-0 bg-black/10 backdrop-blur-sm z-50 flex items-center justify-center">
+          <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        </div>
+      )}
+      {loginLoading && (
+        <div className="fixed inset-0 bg-black/10 backdrop-blur-sm z-50 flex items-center justify-center">
+          <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        </div>
+      )}
+      {registerLoading && (
+        <div className="fixed inset-0 bg-black/10 backdrop-blur-sm z-50 flex items-center justify-center">
+          <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        </div>
+      )}
+       <div className="min-h-screen flex items-center justify-center relative overflow-hidden">
       {/* Animated background */}
       <div className="fixed inset-0 grid-bg opacity-20"></div>
       <div className="gradient-orb gradient-orb-1"></div>
@@ -333,6 +349,7 @@ const Auth = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
